5822deb775ace2ec91360c9077751fe57f155eca
nethra400 <61528928+nethra400@users.noreply.github.com>
2020-06-11 17:41:16 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,31 @@
+package com.capgemini.string.assignment;
+
+public class InsertionSort {
+	public static void insertionSort(int array[]) {
+		int n = array.length;
+		for (int j = 1; j < n; j++) {
+			int key = array[j];
+			int i = j - 1;
+			while ((i > -1) && (array[i] > key)) {
+				array[i + 1] = array[i];
+				i--;
+			}
+			array[i + 1] = key;
+		}
+	}
+
+	public static void main(String a[]) {
+		int[] arr1 = { 4, 8, 71, 1, 6, 5 };
+		System.out.println("Before  Sorting");
+		for (int i : arr1) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+
+		insertionSort(arr1);
+		System.out.println("After Sorting");
+		for (int i : arr1) {
+			System.out.print(i + " ");
+		}
+	}
+}
